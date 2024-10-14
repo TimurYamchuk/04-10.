9e9043435fdda859f4bd72f788534854c3827179
TimurYamchuk <134388477+TimurYamchuk@users.noreply.github.com>
2024-10-14 22:33:05 +0300

    Client.cs

--- HW-04.10_Net-main/Hw_5/Hw_5_/Client.cs
+++ HW-04.10_Net-main/Hw_5/Hw_5_/Client.cs
@@ -1,99 +1,72 @@
-﻿using System;
+using System;
 using static System.Console;
 using Hw_5_CustomDate;
 
-class Hw_5_Clien
+class Hw_5_Client
 {
     static void Main()
     {
         try
         {
-            // Работа с датой по умолчанию
-            Date date1 = new Date();
-            WriteLine("Дата по умолчанию:");
-            date1.PrintDate();
-            WriteLine($"День недели: {date1.Day_Of_Week}");
 
-            // Ввод второй даты
-            WriteLine("\nВведите дату (день месяц год) вводить отдельно:");
+            Date defaultDate = new Date();
+            WriteLine("Дата по умолчанию:");
+            PrintDateInfo(defaultDate);
+
+            WriteLine("\nВведите дату (день месяц год) по отдельности:");
             int day = int.Parse(ReadLine());
             int month = int.Parse(ReadLine());
             int year = int.Parse(ReadLine());
 
-            Date date2 = new Date(day, month, year);
+            Date userDate = new Date(day, month, year);
             WriteLine("Введенная дата:");
-            date2.PrintDate();
-            WriteLine($"День недели: {date2.Day_Of_Week}");
+            PrintDateInfo(userDate);
 
-            // Разница в днях между двумя датами
-            int difference = date1 - date2;
+            int difference = defaultDate - userDate;
             WriteLine($"\nРазница между датами в днях: {difference}");
 
-            // Добавление дней к дате
             WriteLine("\nВведите количество дней для изменения даты:");
             int daysToAdd = int.Parse(ReadLine());
-            date2 = date2 + daysToAdd; // Использование перегруженного оператора "+"
-            WriteLine($"Новая дата после изменения на {daysToAdd} дней:");
-            date2.PrintDate();
-            WriteLine($"День недели: {date2.Day_Of_Week}");
+            userDate += daysToAdd; 
+            WriteLine($"Новая дата после добавления {daysToAdd} дней:");
+            PrintDateInfo(userDate);
 
-            // Использование оператора "++"
-            WriteLine("\nИспользование оператора '++' для добавления одного дня к дате:");
-            date2++;
-            date2.PrintDate();
-            WriteLine($"День недели: {date2.Day_Of_Week}");
+            WriteLine("\nДобавляем один день с помощью оператора '++':");
+            userDate++;
+            PrintDateInfo(userDate);
 
-            // Использование оператора "--"
-            WriteLine("\nИспользование оператора '--' для вычитания одного дня из даты:");
-            date2--;
-            date2.PrintDate();
-            WriteLine($"День недели: {date2.Day_Of_Week}");
+            WriteLine("\nВычитаем один день с помощью оператора '--':");
+            userDate--;
+            PrintDateInfo(userDate);
 
-            // Сравнение дат
             WriteLine("\nСравнение дат:");
-            Date date3 = new Date(15, 8, 2025);
-            WriteLine("Дата 1:");
-            date1.PrintDate();
-            WriteLine("Дата 2:");
-            date2.PrintDate();
-            WriteLine("Дата 3:");
-            date3.PrintDate();
-
-            if (date1 > date2)
-            {
-                WriteLine("Дата 1 больше Даты 2");
-            }
-            else
-            {
-                WriteLine("Дата 1 меньше или равна Дате 2");
-            }
-
-            if (date1 < date3)
-            {
-                WriteLine("Дата 1 меньше Даты 3");
-            }
-
-            if (date1 == date2)
-            {
-                WriteLine("Дата 1 идентична Дате 2");
-            }
-            else
-            {
-                WriteLine("Дата 1 не идентична Дате 2");
-            }
-
-            if (date1 != date2)
-            {
-                WriteLine("Дата 1 не равна Дате 3");
-            }
-            else
-            {
-                WriteLine("Дата 1 равна Дате 3");
-            }
+            Date comparisonDate = new Date(15, 8, 2025);
+            CompareDates(defaultDate, userDate, comparisonDate);
         }
         catch (Exception ex)
         {
             WriteLine("Ошибка: " + ex.Message);
         }
+    }
+
+    static void PrintDateInfo(Date date)
+    {
+        date.PrintDate();
+        WriteLine($"День недели: {date.Day_Of_Week}");
+    }
+
+    static void CompareDates(Date date1, Date date2, Date date3)
+    {
+        WriteLine("Дата 1:");
+        date1.PrintDate();
+        WriteLine("Дата 2:");
+        date2.PrintDate();
+        WriteLine("Дата 3:");
+        date3.PrintDate();
+
+        WriteLine(date1 > date2 ? "Дата 1 больше Даты 2" : "Дата 1 меньше или равна Дате 2");
+        WriteLine(date1 < date3 ? "Дата 1 меньше Даты 3" : "Дата 1 больше или равна Дате 3");
+        WriteLine(date1 == date2 ? "Дата 1 идентична Дате 2" : "Дата 1 не идентична Дате 2");
+        WriteLine(date1 != date3 ? "Дата 1 не равна Дате 3" : "Дата 1 равна Дате 3");
     }
 }
